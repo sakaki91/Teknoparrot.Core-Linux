cbf98b3f1061773972ba7978c302b0f1a9e44fe3
sakaki <91630610+sakaki91@users.noreply.github.com>
2026-04-15 19:45:51 -0300

    Update 3.2
    The flag system has been reworked, and now when you run ./install --help, it shows your flags and new installation types, including --custom-dir (which was already there, for custom directories) and --last-dir, which uses the last directory used by the script, precisely to facilitate maintenance or reinstallation of the wrapper. It also has a --debug flag that generates a log file in ~/, and --debug is also present as a subflag in custom-dir and last-dir (however, last-dir also has a --watch subflag, to see the last directory used without touching the current installation).

--- install.sh
+++ install.sh
@@ -24,48 +24,48 @@ dependencyInstall(){
     clear
     export WINEPREFIX=${PREFIX}
     printf "$NORMAL_LOG Wineboot. ($PREFIX)" && wineboot -u &> $AWL_LOG
-        if [ -d "$PREFIX"/drive_c ]; then
-            printf " $DONE_LOG\n$NORMAL_LOG Downloading dependencies."
-            wget -c https://aka.ms/dotnet/8.0/dotnet-runtime-win-x64.exe --directory-prefix="$TMP" &>> $AWL_LOG
-            wget -c https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.exe --directory-prefix="$TMP" &>> $AWL_LOG
-            wget -c https://download.microsoft.com/download/8/4/a/84a35bf1-dafe-4ae8-82af-ad2ae20b6b14/directx_Jun2010_redist.exe --directory-prefix="$TMP" &>> $AWL_LOG
-            wget -c https://github.com/doitsujin/dxvk/releases/download/v$DXVK_VERSION/dxvk-$DXVK_VERSION.tar.gz --directory-prefix="$TMP" &>> $AWL_LOG
-            [ ! -f "$TMP"/dotnet-runtime-win-x64.exe ] && printf " $ERROR_LOG\n" && exit
-            [ ! -f "$TMP"/windowsdesktop-runtime-win-x64.exe ] && printf " $ERROR_LOG\n" && exit
-            [ ! -f "$TMP"/directx_Jun2010_redist.exe ] && printf " $ERROR_LOG\n" && exit
-            [ ! -f "$TMP"/dxvk-$DXVK_VERSION.tar.gz ] && printf " $ERROR_LOG\n" && exit
-            printf " $DONE_LOG\n$NORMAL_LOG Installing dependencies."
-            wine "$TMP"/dotnet-runtime-win-x64.exe /install /quiet /norestart &>> $AWL_LOG
-            wine "$TMP"/windowsdesktop-runtime-win-x64.exe /install /quiet /norestart &>> $AWL_LOG
-            wine "$TMP"/directx_Jun2010_redist.exe /Q /C /T:"C:\TMP" &>> $AWL_LOG
-            wine "$TMP"/DXSETUP.exe /silent &>> $AWL_LOG
-            tar -xf "$TMP"/dxvk-$DXVK_VERSION.tar.gz --directory "$TMP"
-            mv "$TMP"/dxvk-$DXVK_VERSION/x32/*.dll "$PREFIX"/drive_c/windows/syswow64
-            mv "$TMP"/dxvk-$DXVK_VERSION/x64/*.dll "$PREFIX"/drive_c/windows/system32
-            wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d10core /d native,builtin /f &>> $AWL_LOG
-            wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d11 /d native,builtin /f &>> $AWL_LOG
-            wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d8 /d native,builtin /f &>> $AWL_LOG
-            wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d9 /d native,builtin /f &>> $AWL_LOG
-            wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v dxgi /d native,builtin /f &>> $AWL_LOG
-            printf " $DONE_LOG\n$NORMAL_LOG Downloading TeknoParrot (Web-Installer)."
-            wget -c https://github.com/nzgamer41/TPBootstrapper/releases/latest/download/TPBootstrapper.zip --directory-prefix="$TMP" &>> $AWL_LOG
-            if [ -f "$TMP"/TPBootstrapper.zip ]; then
-                (
-                unzip "$TMP"/TPBootstrapper.zip -d "$PROGRAM" &>> $AWL_LOG
-                cd "$PROGRAM"
-                wine TPBootstrapper.exe &>> $AWL_LOG
-                [ -f "$PROGRAM"/TeknoParrotUi.exe ] && printf " $DONE_LOG\n"
-                [ ! -f "$PROGRAM"/TeknoParrotUi.exe ] && printf " $ERROR_LOG\n" && exit
-                )
-            else
-                printf "$ERROR_LOG\n" && exit
-            fi
-            ls $TREE &>> $AWL_LOG && ls $TREE/* &>> $AWL_LOG
-            rm -rf "$PROGRAM"/TPBootstrapper*
-            rm -rf "$TMP" && printf "$NORMAL_LOG Temporary files cleared. $DONE_LOG\n"
-       else
-            printf " $ERROR_LOG\n" && exit
-       fi
+    if [ -d "$PREFIX"/drive_c ]; then
+        printf " $DONE_LOG\n$NORMAL_LOG Downloading dependencies."
+        wget -c https://aka.ms/dotnet/8.0/dotnet-runtime-win-x64.exe --directory-prefix="$TMP" &>> $AWL_LOG
+        wget -c https://aka.ms/dotnet/8.0/windowsdesktop-runtime-win-x64.exe --directory-prefix="$TMP" &>> $AWL_LOG
+        wget -c https://download.microsoft.com/download/8/4/a/84a35bf1-dafe-4ae8-82af-ad2ae20b6b14/directx_Jun2010_redist.exe --directory-prefix="$TMP" &>> $AWL_LOG
+        wget -c https://github.com/doitsujin/dxvk/releases/download/v$DXVK_VERSION/dxvk-$DXVK_VERSION.tar.gz --directory-prefix="$TMP" &>> $AWL_LOG
+        [ ! -f "$TMP"/dotnet-runtime-win-x64.exe ] && printf " $ERROR_LOG\n" && exit
+        [ ! -f "$TMP"/windowsdesktop-runtime-win-x64.exe ] && printf " $ERROR_LOG\n" && exit
+        [ ! -f "$TMP"/directx_Jun2010_redist.exe ] && printf " $ERROR_LOG\n" && exit
+        [ ! -f "$TMP"/dxvk-$DXVK_VERSION.tar.gz ] && printf " $ERROR_LOG\n" && exit
+        printf " $DONE_LOG\n$NORMAL_LOG Installing dependencies."
+        wine "$TMP"/dotnet-runtime-win-x64.exe /install /quiet /norestart &>> $AWL_LOG
+        wine "$TMP"/windowsdesktop-runtime-win-x64.exe /install /quiet /norestart &>> $AWL_LOG
+        wine "$TMP"/directx_Jun2010_redist.exe /Q /C /T:"C:\TMP" &>> $AWL_LOG
+        wine "$TMP"/DXSETUP.exe /silent &>> $AWL_LOG
+        tar -xf "$TMP"/dxvk-$DXVK_VERSION.tar.gz --directory "$TMP"
+        mv "$TMP"/dxvk-$DXVK_VERSION/x32/*.dll "$PREFIX"/drive_c/windows/syswow64
+        mv "$TMP"/dxvk-$DXVK_VERSION/x64/*.dll "$PREFIX"/drive_c/windows/system32
+        wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d10core /d native,builtin /f &>> $AWL_LOG
+        wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d11 /d native,builtin /f &>> $AWL_LOG
+        wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d8 /d native,builtin /f &>> $AWL_LOG
+        wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v d3d9 /d native,builtin /f &>> $AWL_LOG
+        wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v dxgi /d native,builtin /f &>> $AWL_LOG
+        printf " $DONE_LOG\n$NORMAL_LOG Downloading TeknoParrot (Web-Installer)."
+        wget -c https://github.com/nzgamer41/TPBootstrapper/releases/latest/download/TPBootstrapper.zip --directory-prefix="$TMP" &>> $AWL_LOG
+        if [ -f "$TMP"/TPBootstrapper.zip ]; then
+        (
+            unzip "$TMP"/TPBootstrapper.zip -d "$PROGRAM" &>> $AWL_LOG
+            cd "$PROGRAM"
+            wine TPBootstrapper.exe &>> $AWL_LOG
+            [ -f "$PROGRAM"/TeknoParrotUi.exe ] && printf " $DONE_LOG\n"
+            [ ! -f "$PROGRAM"/TeknoParrotUi.exe ] && printf " $ERROR_LOG\n" && exit
+        )
+        else
+            printf "$ERROR_LOG\n" && exit
+        fi
+        ls $TREE &>> $AWL_LOG && ls $TREE/* &>> $AWL_LOG
+        rm -rf "$PROGRAM"/TPBootstrapper*
+        rm -rf "$TMP" && printf "$NORMAL_LOG Temporary files cleared. $DONE_LOG\n"
+    else
+        printf " $ERROR_LOG\n" && exit
+    fi
 }
 
 executableCreation(){
@@ -74,31 +74,33 @@ executableCreation(){
         HEADER="#!/bin/bash"
         EXEC="LC_ALL=en_US.UTF-8 LC_NUMERIC=en_US.UTF-8 LANG=en_US.UTF-8 WINEPREFIX=$PREFIX wine $PROGRAM/TeknoParrotUi.exe"
         echo $HEADER > TeknoParrot
-        echo $EXEC >> TeknoParrot 
+        echo $EXEC >> TeknoParrot
         chmod +x TeknoParrot
     )
 }
 
-AWL_NAME="Arcade Wrapper Linux/Unix-like"
-AWL_VERSION="3.1-9"
-AWL_LOG="/dev/null"
-DONE_LOG="\e[1;32mOK\033[0m"
-NORMAL_LOG="\e[1;34m*\033[0m"
-ERROR_LOG="\e[1;31mERROR\033[0m"
+AWL_NAME="Arcade Wrapper Linux/Unix-like" && AWL_VERSION="3.2" && AWL_LOG="/dev/null"
+DEFAULT_COLOR="\033[0m" && GRAY_COLOR="\e[0;37m" && BLACK_BG="\e[0;40m" && DONE_LOG="\e[1;32mOK\033[0m" && NORMAL_LOG="\e[1;34m*\033[0m" && ERROR_LOG="\e[1;31mERROR\033[0m"
 TREE=${HOME}/TeknoParrot
 DXVK_VERSION="2.7.1"
 
 case $1 in
     "--help")
-        echo -e "\n$AWL_NAME $AWL_VERSION\n\n--help\t\tShow this message.\n--version\tShow wrapper version.\n--custom-dir\tWith this flag you can choose a custom installation directory.\n--debug \tThis executes the script and generates a log file (AWL.LOG) in $HOME.\n"
+        echo -e "\n$AWL_NAME $AWL_VERSION\n\n--help\t\tShow this message.\n--version\tShow wrapper version.\n\n$BLACK_BG./install @args @subargs$DEFAULT_COLOR\n\n@args:\n--debug\t\tThis executes the script and generates a log file (AWL.LOG) in $HOME.\n--last-dir\tUses the last directory used in the script (ideal for reinstalls/maintenance).\n--custom-dir\tWith this flag you can choose a custom installation directory.\n\n$GRAY_COLOR@subargs:\n-watch\t\tShows the last directory used, without installing anything.\n-debug \t\tThe -debug option serves as an additional subargument, for debug in the default directory, simply use --debug.\n$DEFAULT_COLOR\n"
         exit
+    ;;
+    "--last-dir")
+        source src/lastpath
+        [[ $2 == "-watch" ]] && echo -e "Last path used: $LASTPATH" && exit
+        [[ $2 == "-debug" ]] && AWL_LOG=$HOME/awl.log
+        TREE="$LASTPATH"
     ;;
     "--custom-dir")
         TREE=$(zenity --file-selection --directory --title "Select your desired directory:")/TeknoParrot
-        [[ $2 == "--debug" ]] && AWL_LOG=$HOME/awl.log
+        [[ $2 == "-debug" ]] && AWL_LOG=$HOME/awl.log
     ;;
     "--debug")
-        AWL_LOG=$HOME/AR.LOG
+        AWL_LOG=$HOME/AWL.LOG
     ;;
     "--version")
         echo -e "$AWL_NAME $AWL_VERSION"
@@ -106,6 +108,8 @@ case $1 in
     ;;
 esac
 
+FINAL_PATH="$TREE"
+mkdir -p src/ && echo LASTPATH=$FINAL_PATH > src/lastpath
 hostDependencyChecker
 clear
 atomicTree
